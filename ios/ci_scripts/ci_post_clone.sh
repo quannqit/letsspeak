a6cfffbcdd7bc407d4149de1d7c601872406b11c
Quan_Quoc.N <quan_quoc.n@kbtgvn.tech>
2024-02-14 14:56:36 +0700

    add ci_post_clone.sh

---
+++ ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Fail this script if any subcommand fails.
+set -e
+
+# The default execution directory of this script is the ci_scripts directory.
+cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
+
+echo "API_HOST=$API_HOST" > .env
+
+# Install Flutter using git.
+git clone -b master https://github.com/flutter/flutter.git $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
+
+# Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
+flutter precache --ios
+
+# Install Flutter dependencies.
+flutter pub get
+
+# Install CocoaPods using Homebrew.
+HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
+brew install cocoapods
+
+# Install CocoaPods dependencies.
+cd ios && pod install # run `pod install` in the `ios` directory.
+
+exit 0
