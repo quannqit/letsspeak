0a079de597597a905626254d00bfb404c28cbd65
Quan_Quoc.N <quan_quoc.n@kbtgvn.tech>
2024-02-15 22:57:49 +0700

    Use Flutter version 3.12.0

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -9,8 +9,13 @@ cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cl
 echo "API_HOST=$API_HOST" > .env
 
 # Install Flutter using git.
-git clone -b 3.12.0 https://github.com/flutter/flutter.git $HOME/flutter
+git clone https://github.com/flutter/flutter.git $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
+
+cd $HOME/flutter
+git checkout 3.12.0
+
+cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
 
 # Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
 flutter precache --ios
@@ -21,8 +26,9 @@ flutter pub get
 # Install CocoaPods using Homebrew.
 HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
 brew install cocoapods
+pod --version
 
 # Install CocoaPods dependencies.
-cd ios && pod install # run `pod install` in the `ios` directory.
+cd $CI_PRIMARY_REPOSITORY_PATH/ios && pod install # run `pod install` in the `ios` directory.
 
 exit 0
